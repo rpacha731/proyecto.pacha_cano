2f58bd0deef40ad9c56e99792a818b5486175e05
Leonel Pacha <rpacha731@alumnos.iua.edu.ar>
2021-11-09 12:46:40 -0300

    initial commit

---
+++ src/test/java/com/iua/iw3/proyecto/pacha_cano/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iua.iw3.proyecto.pacha_cano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
